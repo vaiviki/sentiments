f5c5990540b1c3c3abd1f77e7ce64aecafd46be2
virali ganatra <virali232@gmail.com>
2024-02-28 11:42:13 +0530

    added os

--- senti/sentimentanalysis.py
+++ senti/sentimentanalysis.py
@@ -4,6 +4,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score, classification_report
+import os
 
 # Load the dataset
 file_path = os.path.join(os.getcwd(), 'IMDB_Dataset.csv')
